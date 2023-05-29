16522f79fb390ddcf926d84a0cef8973960f672f
Gaurav Bhatnagar <gauran.bhatnagar@betterplace.co.in>
2023-05-29 13:03:44 +0530

    UI changes

--- src/pages/Settings/Modals/SBT/index.tsx
+++ src/pages/Settings/Modals/SBT/index.tsx
@@ -1,14 +1,20 @@
-import { Box, Typography, Grid, Button } from "@mui/material"
+import { Box, Typography, Grid, Button, Menu, MenuItem } from "@mui/material"
 import React, { useState } from "react";
+import { get as _get } from 'lodash'
+import { KeyboardArrowDown } from '@mui/icons-material';
 import IconButton from "components/IconButton"
 import CloseSVG from 'assets/svg/close-new.svg'
 import rectangleSbt from 'assets/svg/group265.svg'
 import greyStar from 'assets/svg/greySmallStar.svg'
 import greyGroup from 'assets/svg/greySmallGroup.svg'
+import debitCreditFull from 'assets/svg/debitCreditFull.svg'
+import debitCreditTransparent from 'assets/svg/debitCreditTransparent.svg'
 import orangeUploadIcon from 'assets/svg/orangeUploadIcon.svg'
 import invertedSBTSquare from 'assets/svg/invertedSBTSquare.svg'
+import ploygon from 'assets/svg/ploygon.svg'
 import TextInput from "components/TextInput"
-import Select from "components/Select"
+import { SUPPORTED_CHAIN_IDS, SupportedChainId } from 'constants/chains';
+import { CHAIN_INFO } from 'constants/chainInfo'
 import { makeStyles } from '@mui/styles';
 
 const useStyles = makeStyles((theme: any) => ({
@@ -76,19 +82,58 @@ const useStyles = makeStyles((theme: any) => ({
     sbtIcon: {
         marginRight: 5,
         marginBottom: 4
+    },
+    orangeUploadButton: {
+        marginLeft: 10,
+        background: 'linear-gradient(180deg, #FBF4F2 0%, #EEF1F5 100%)',
+        width: 25,
+        height: 25,
+        borderRadius: 2,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        textAlign: 'center'
+    },
+    tableContainer: {
+        maxHeight: 811,
+        overflow: 'scroll'
+    },
+    select: {
+        background: '#FFF',
+        borderRadius: '10px !important',
+        boxShadow: 'none !important',
+        fontSize: '16px !important',
+        minWidth: 'inherit !importnt',
+        padding: '0px !important'
+    },
+    requestPaymentButton: {
+        background: '#fff !important',
+        color: '#B12F15 !important',
+        fontSize: '16px !important',
+        boxShadow: '3px 5px 4px rgba(27, 43, 65, 0.05), -3px -3px 8px rgba(201, 75, 50, 0.1) !important',
+        borderRadius: '5px !important',
+        width: '177px !important',
+        height: '40px !important',
+        padding: '0px !important',
+        lineHeight: '18px !important',
+        letterSpacing: '-1.1% !important',
     }
 }));
-
 
 const SbtModal = ({ onClose }: { onClose: any }) => {
     const classes = useStyles();
     const [name, setName] = useState('');
-    const tableData = [{ status: 'Awaiting Payment', time: null, amount: '120 ETH'},{ status: 'Paid', time: '10/20 10:33', amount: '120 ETH'}]
+    const tableData = [{ status: 'Awaiting Payment', time: null, amount: '120 ETH' }, { status: 'Paid', time: '10/20 10:33', amount: '120 ETH' }]
     const currencies = [{ name: '24 SWEAT', icon: greyStar }, { name: '240 Bv', icon: '' }, { name: '240 Tok', icon: '' }, { name: '240 RhO', icon: '' }, { name: '240 Eth', icon: '' }]
     const [images, setImage] = useState(['https://loremflickr.com/320/240?lock=212', 'https://loremflickr.com/320/240?lock=30976', 'https://loremflickr.com/320/240?lock=1']);
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event: React.MouseEvent<HTMLElement>) => setAnchorEl(event.currentTarget);
+    const handleClose = () => setAnchorEl(null);
+    const [currentChain, setCurrentChain] = useState(SupportedChainId.GOERLI)
 
     return (
-        <Box sx={{ width: 930, flex: 1, paddingBottom: '80px', paddingLeft: '3px', borderRadius: '20px 0px 0px 20px' }}>
+        <Box sx={{ width: 786, flex: 1, paddingBottom: '80px', paddingLeft: '3px', borderRadius: '20px 0px 0px 20px' }}>
             <IconButton sx={{ position: 'fixed', right: 32, top: 32 }} onClick={onClose}>
                 <img src={CloseSVG} />
             </IconButton>
@@ -103,7 +148,7 @@ const SbtModal = ({ onClose }: { onClose: any }) => {
                     placeholder="myname" sx={{ my: 2, width: 350 }} label="Name" />
             </Box>
             <Box>
-                <Typography mt={4.5} className={classes.textInput}>My SBTs</Typography>
+                <Typography mt={2.5} className={classes.textInput}>My SBTs</Typography>
                 <Grid display='flex' flexDirection="row" position="relative" paddingY={2}>
                     {images.map((url: string, index: number) => {
                         return <Box paddingLeft={index > 0 ? 2.5 : 0}><Box className={classes.imageContainer}>
@@ -118,52 +163,81 @@ const SbtModal = ({ onClose }: { onClose: any }) => {
                 </Grid>
             </Box>
             <Grid container display='flex' flexDirection="row" justifyContent='flex-start' my={4}>
-                <Box display='flex' flexDirection="row" alignContent='center'>
-                    <Typography className={classes.textInput}>My Earnings</Typography>
-                    <Select options={['polygon']} selected={0} setSelectedValue={() => { }} selectStyle={{ borderRadius: '0px 10px 10px 0px', borderWidth: 1, borderLeftWidth: 0, borderColor: 'rgba(27, 43, 65, 0.1)', boxShadow: 'inset -1px 0px 4px rgba(27, 43, 65, 0.1)', height: 50, padding: '0px 50px 0px 20px' }} />
-                    <Button variant="outlined" sx={{ width: 77, height: 40, fontSize: 16 }}>Request Payment</Button>
-                </Box>
+                <Grid display='flex' flexDirection="row" alignItems='center' justifyContent='space-between' width={'100%'} mb={2.5}>
+                    <Grid display='flex' flexDirection="row" alignItems='center'>
+                        <Typography className={classes.textInput}>My Earnings</Typography>
+                        <Button onClick={handleClick} aria-controls={open ? 'fade-menu' : undefined} aria-haspopup="true" aria-expanded={open ? 'true' : undefined} className={classes.select} variant="contained" color="secondary" disableElevation startIcon={<img style={{ width: 18, height: 18 }} src={_get(CHAIN_INFO, `${currentChain}.logoUrl`)} />} endIcon={<KeyboardArrowDown />}>
+                            {_get(CHAIN_INFO, `${currentChain}.label`)}
+                        </Button>
+                        <Menu
+                            MenuListProps={{
+                                'aria-labelledby': 'fade-button',
+                            }}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'right',
+                            }}
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            anchorEl={anchorEl}
+                            open={open}
+                            onClose={handleClose}
+                        >
+                            {
+                                SUPPORTED_CHAIN_IDS.map(sc =>
+                                    <MenuItem style={{ textTransform: 'uppercase' }} onClick={() => { handleClose() }}>
+                                        <img style={{ marginRight: '8px', width: 18, height: 18 }} src={CHAIN_INFO[sc].logoUrl} />{CHAIN_INFO[sc].label}</MenuItem>)
+                            }
+                        </Menu>
+                    </Grid>
+                    <Button variant="contained" className={classes.requestPaymentButton}>Request Payment</Button>
+                </Grid>
                 <Grid container display='flex' flexDirection="row" justifyContent="space-between" className={classes.sweatContainer}>
                     <Grid item display='flex' flexDirection="row">{currencies.map((currency) => {
                         return <Grid item display='flex' flexDirection="row" paddingLeft={2}>
-                            <img src={currency.icon} style={{ marginRight: 9}}/>
+                            <img src={currency.icon} style={{ marginRight: 9 }} />
                             <Typography className={classes.textInput}>{currency.name}</Typography>
                         </Grid>
                     })}
                     </Grid>
                     <Grid item paddingLeft={2} display='flex' flexDirection="row">
-                        <img src={greyGroup} style={{ marginRight: 6}}/>
-                        <Typography className={classes.textInput} color="#188C7C" style={{ marginRight: 6}}>$8,34</Typography>
+                        <img src={greyGroup} style={{ marginRight: 6 }} />
+                        <Typography className={classes.textInput} color="#188C7C" style={{ marginRight: 6 }}>$8,34</Typography>
                         <Typography className={classes.textInput}>total</Typography>
                     </Grid>
                 </Grid>
-                <Grid container display='flex' flexDirection="column" justifyContent="space-between" my={3}>
-                   {tableData.map((data: any, index: number)=>{
-                   return <Grid display='flex' flexDirection="row" justifyContent="space-between" sx={{ opacity: index>0? 1: 0.5}}>
-                    <Grid item display="flex" flexDirection="row">
-                        <Box display="flex" flexDirection="row" padding={2}>
-                            <img src={greyGroup} style={{ marginRight: 8}}/>
-                            <Typography>{data.amount} /</Typography>
-                            <img src={''}/>
-                        </Box>
-                        <Box>   
-                            <Typography className={classes.textInput}>Invoice | UI improvements</Typography>
-                            <Typography>from Name of Organisation</Typography>
-                        </Box>
-                    </Grid>
-                    <Grid display='flex' flexDirection="row" justifyContent="flex-end">
-                    <Box>
-                        <Typography className={classes.textInput}>{data.status}</Typography>
-                        <Typography>{data.time || '...'}</Typography>
-                    </Box>
-                    <img src={orangeUploadIcon} width={25} height={25}/>
-                    </Grid>
-                   </Grid>
-                   })
-                }
+                <Grid container display='flex' flexDirection="column" justifyContent="space-between" my={3} className={classes.tableContainer}>
+                    {tableData.map((data: any, index: number) => {
+                        return <Grid display='flex' flexDirection="row" justifyContent="space-between" sx={{ opacity: index > 0 ? 1 : 0.5 }}>
+                            <Grid item display="flex" flexDirection="row" alignItems='center'>
+                                <Box display="flex" flexDirection="row" padding={2}>
+                                    <img src={index > 0 ? debitCreditFull : debitCreditTransparent} style={{ marginRight: 8 }} />
+                                    <Typography>{data.amount}</Typography>
+                                    <Typography sx={{paddingX: 1}}>/</Typography>
+                                    <img src={ploygon} />
+                                </Box>
+                                <Box>
+                                    <Typography className={classes.textInput}>Invoice | UI improvements</Typography>
+                                    <Typography>from Name of Organisation</Typography>
+                                </Box>
+                            </Grid>
+                            <Grid display='flex' flexDirection="row" justifyContent="center" alignItems="center">
+                                <Grid display='flex' flexDirection="column" justifyContent="flex-end" alignItems="center" sx={{float: 'right'}}>
+                                    <Typography className={classes.textInput}>{data.status}</Typography>
+                                    <Typography>{data.time || '...'}</Typography>
+                                </Grid>
+                                {index > 0 ? <Box className={classes.orangeUploadButton}>
+                                    <img src={orangeUploadIcon} width={15} height={15} />
+                                </Box> : <Box width={25}></Box>}
+                            </Grid>
+                        </Grid>
+                    })
+                    }
                 </Grid>
             </Grid>
-        </Box >
+        </Box>
     )
 }
 
